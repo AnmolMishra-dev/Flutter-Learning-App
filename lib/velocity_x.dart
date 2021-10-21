00c535dd7861d4188d8623ef9ff3fbcf8080e169
Anmol Mishra <tbsdev3@gmail.com>
2021-10-21 15:24:25 +0530

    velocity X

--- lib/velocity_x.dart
+++ lib/velocity_x.dart
@@ -11,7 +11,60 @@ class Velocity_xHome extends StatefulWidget {
 }
 
 class _Velocity_xHomeState extends State<Velocity_xHome> {
-  List velocity=[];
+  List velocity=[
+    {
+      "heading": "What is Flutter Velocity X?",
+      "content": "The Velocity X  library in flutter development is a minimalist flutter framework that helps you in building custom Application design Rapidly.\n\nVelocity X comes with various properties that we can apply to our flutter widget such as Color, Padding, Text , Box, Card, SizedBox and many more.",
+      "imageshow": false,
+      "image": ""
+    },
+    {
+      "heading": "Installation of Velocity X in Flutter Project",
+      "content": "",
+      "imageshow": false,
+      "image": ""
+    },
+    {
+      "heading": "Adding Dependencies",
+      "content": "Add the following dependencies in your pubspec.yaml file.",
+      "imageshow": true,
+      "image": "https://drive.google.com/uc?export=view&id=1EmJzX0cgzvBaTMwuCa5dJTORFdQ5lJi-"
+    },
+    {
+      "heading": " Importing the VelocityX package",
+      "content": "Once you have added the velocityx dependencies, Now you can use them in your dart code (main.dart), just by importing the package / component that are required for application development.",
+      "imageshow": true,
+      "image": "https://drive.google.com/uc?export=view&id=153Xdr6WKYOIhnV6eanmpz3ynb6OTTu1i"
+    },
+    {
+      "heading": "How to use Velocity X in Text Widget.",
+      "content": "This Library gives super power to Text\n\nNote : When are are done with applying velocityx properties to widget or string end it with .make() to make it back to flutter widget.",
+      "imageshow": true,
+      "image": "https://drive.google.com/uc?export=view&id=1C_WRMjr0aLhO8Xe2ylSxYCEkBaBXBtcW"
+    },
+    {
+      "heading": "Padding to the VelocityX card",
+      "content": "This will set a Padding to the card with padding size of 16 px vertically.",
+      "imageshow": true,
+      "image": "https://drive.google.com/uc?export=view&id=1AfDU5Vm13lSARyub4eD-bALckbkS63SA"
+    },
+    {
+      "heading": "Velocity x Card",
+      "content": "You can use card properties on any of the flutter widgets & wrap it to a card widget.",
+      "imageshow": true,
+      "image": "https://drive.google.com/uc?export=view&id=1OHpMR99z5hAfpYkp9e0ztY5zRW5IU-dn"
+    },
+    {
+      "heading": "Setting Color using Hex Code",
+      "content": "you can even set the card color using Hex Color Code properties.",
+      "imageshow": true,
+      "image": "https://drive.google.com/uc?export=view&id=1wve-6_fDGaxKyWNoNdZ4QImyeXLDcsQB"
+    }
+
+
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
