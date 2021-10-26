499f9a25c10d919dd40b078434ed3c14f8d6695b
Anmol Mishra <tbsdev3@gmail.com>
2021-10-26 11:12:08 +0530

    redx

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/redx.dart';
 
 
 import 'Database/hive.dart';
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
 
 
       ),
-      home: const Home()
+      home: const RedxHome()
     );
   }
 }
